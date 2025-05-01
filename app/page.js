ed8144b9e23937eb5fe0a819f265c7ac9a0ec975
Kevin <mediaman502@aol.com>
2025-05-01 14:06:59 -0400

    transitioning to the next page

--- app/page.js
+++ app/page.js
@@ -1,30 +1,37 @@
+"use client"
 import Nav from "./components/Nav";
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [hovered, isHovered] = useState(false)
+
   return (
     <main className="min-h-screen flex flex-col">
       <Nav />
-
       <div className="flex-1 flex relative">
         <div className="flex-grow flex justify-between items-center">
         <div className="relative">
           <Image src="/images/Rectangle 2779.png" width={300} height={400} alt="right square" />
-          <div className="absolute top-1/2 right-28 -translate-y-1/2 flex items-center space-x-2 text-[clamp(0.65rem,2vw,1rem)]">
-            <button className="border rotate-45 w-10 h-10 flex items-center justify-center">
+          <div className="absolute top-1/2 right-28 -translate-y-1/2 flex items-center space-x-2 text-[clamp(0.65rem,2vw,1rem)] david">
+            <button className="border rotate-45 w-10 h-10 flex items-center justify-center cursor-pointer">
               <span className="block -rotate-287">◀</span>
             </button>
-            <span className="pl-3">DISCOVER A.I.</span>
+            <span className="pl-3 lucas">DISCOVER A.I.</span>
           </div>
         </div>
           <div>
-            <h1 className="text-[clamp(2.5rem,10vw,8rem)] text-center leading-tight font-light tracking-tighter">Sophisticated <br/> skincare</h1>
+            <h1 className={`text-[clamp(2.5rem,10vw,8rem)] text-center leading-tight font-light tracking-tighter transition-all ${hovered ? "-translate-x-[35rem] duration-300" : ""}`}>
+              Sophisticated <br/> skincare
+            </h1>
           </div>
           <div className="relative">
             <Image src="/images/Rectangle 2778.png" width={300} height={400} alt="left square" />
-            <div className="absolute top-1/2 left-28 -translate-y-1/2 flex items-center space-x-2 text-[clamp(0.65rem,2vw,1rem)]">
-              <span className="pr-3">TAKE TEST</span>
-              <button className="border rotate-45 w-10 h-10 flex items-center justify-center">
+            <div className="absolute top-1/2 left-28 -translate-y-1/2 flex items-center space-x-2 text-[clamp(0.65rem,2vw,1rem)] group"
+              onMouseEnter={() => (isHovered(true))} onMouseLeave={() => (isHovered(false))}
+            >
+              <span className="pr-3 transition-transform duration-300 group-hover:-translate-x-[25px]">TAKE TEST</span>
+              <button className="border rotate-45 w-10 h-10 flex items-center justify-center cursor-pointer">
                 <span className="-block -rotate-287">▶</span>
               </button>
             </div>
